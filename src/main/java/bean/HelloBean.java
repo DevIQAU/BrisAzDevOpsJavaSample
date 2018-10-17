c096c8d8961fb3a7db7e95d4b26203fd7cd74229
Richard Angus <richard.angus@live.com.au>
2018-10-17 11:55:32 +1000

    Changed the source

---
+++ src/main/java/bean/HelloBean.java
@@ -0,0 +1,23 @@
+package bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean  implements Serializable{
+
+	private static final long serialVersionUID = -4823295172962937652L;
+	
+	private String message = "Your Java app is up and running on Azure. This will have been deployed to azure as well after this update";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}	
+}
